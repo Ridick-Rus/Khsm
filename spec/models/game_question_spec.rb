65f01f8bb08054c947318b16483acdcb2d04b0b3
andrey <litomon275@yandex.ru>
2023-03-27 16:38:21 +0300

    fix

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -5,6 +5,29 @@ RSpec.describe GameQuestion, type: :model do
     FactoryGirl.create(:game_question, a: 2, b: 1, c: 4, d: 3)
   end
 
+  describe '#correct_answer_key' do
+    it 'returns correct answer key' do
+      expect(game_question.correct_answer_key).to eq('b')
+    end
+  end
+
+  describe '#add_friend_call' do
+    before { game_question.add_friend_call }
+    let!(:call) { game_question.help_hash[:friend_call] }
+
+    it 'help_hash includes fifty_fifty' do
+      expect(game_question.help_hash).to include(:friend_call)
+    end
+
+    it 'return string' do
+      expect(call).to be_a(String)
+    end
+
+    it 'contains answer key' do
+      expect(call).to include('вариант')
+    end
+  end
+
   context 'game status' do
     it 'correct .variants' do
       expect(game_question.variants).to eq(
@@ -22,12 +45,6 @@ RSpec.describe GameQuestion, type: :model do
     it 'correct .level & .text delegates' do
       expect(game_question.text).to eq(game_question.question.text)
       expect(game_question.level).to eq(game_question.question.level)
-    end
-
-    describe 'correct_answer_key' do
-      it 'returns correct answer key' do
-        expect(game_question.correct_answer_key).to eq('b')
-      end
     end
 
     # help_hash у нас имеет такой формат:
@@ -79,28 +96,5 @@ RSpec.describe GameQuestion, type: :model do
 
     expect(ff).to include('b') # должен остаться правильный вариант
     expect(ff.size).to eq 2 # всего должно остаться 2 варианта
-  end
-
-  describe '#correct_answer_key' do
-    it 'returns correct answer key' do
-      expect(game_question.correct_answer_key).to eq('b')
-    end
-  end
-
-  describe '#add_friend_call' do
-    before { game_question.add_friend_call }
-    let!(:call) { game_question.help_hash[:friend_call] }
-
-    it 'help_hash includes fifty_fifty' do
-      expect(game_question.help_hash).to include(:friend_call)
-    end
-
-    it 'return string' do
-      expect(call).to be_a(String)
-    end
-
-    it 'contains answer key' do
-      expect(call).to include('вариант')
-    end
   end
 end
